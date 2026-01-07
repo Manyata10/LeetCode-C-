42088c3c69aff467b27db33b8ce731cde4781501
Manyata10 <130667631+Manyata10@users.noreply.github.com>
2026-01-08 01:45:56 +0530

    Time: 1211 ms (28.36%), Space: 0B (100%) - LeetHub

--- 1378-replace-employee-id-with-the-unique-identifier/1378-replace-employee-id-with-the-unique-identifier.sql
+++ 1378-replace-employee-id-with-the-unique-identifier/1378-replace-employee-id-with-the-unique-identifier.sql
@@ -1,4 +1,9 @@
-SELECT eu.unique_id, e.name
-FROM EmployeeUNI eu 
-RIGHT JOIN Employees e 
+SELECT unique_id, name
+FROM Employees e 
+LEFT JOIN EmployeeUNI eu 
 ON eu.id=e.id;
+
+-- SELECT unique_id, name
+-- FROM Employees as A
+-- left JOIN EmployeeUNI as B
+-- ON A.id = B.id;
