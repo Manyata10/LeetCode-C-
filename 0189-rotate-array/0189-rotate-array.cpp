e5d6267f6da78c1679e14502d589a5e2df764415
Manyata <130667631+Manyata10@users.noreply.github.com>
2026-07-24 19:16:44 +0530

    Time: 0 ms (100%), Space: 30 MB (28.49%) - LeetHub

--- 0189-rotate-array/0189-rotate-array.cpp
+++ 0189-rotate-array/0189-rotate-array.cpp
@@ -1,14 +1,28 @@
 class Solution {
 public:
-    void rotate(vector<int>& nums, int k) {
+    void rotate(vector<int>& arr, int d) {
         //if mod 10 the ans will be 0-9
         // if mod n then asn will be 0-n-1
         //n+k%n
-        int n= nums.size();
-        vector<int> temp(n);
-        for(int i=0;i<n;i++){
-            temp[(i+k)%n]=nums[i];
+        //BETTER APPROACH
+        int n = arr.size();
+        // vector<int> temp(n);
+        // for(int i=0;i<n;i++){
+        //     temp[(i+k)%n]=nums[i];
+        // }
+        // nums=temp;
+        ///OPTIMAL APPROACH
+        d = d % n;
+
+        vector<int> temp(d);
+        for(int i=n-d; i<n; i++){ //suppose d = 3; 
+            temp[i-(n-d)]=arr[i];  //stored 0 to 2 index elements = 3
+        }                     //so start from index 3 (4th element)
+        for(int i=n-d-1; i>=0; i--){
+            arr[i+d]=arr[i];
         }
-        nums=temp;
+        for(int i=0;i<d;i++){
+            arr[i]=temp[i];
+        }
     }
 };
