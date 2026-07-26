d0189eb212896ac7f96e663e5dd0e0f254c3190c
Manyata <130667631+Manyata10@users.noreply.github.com>
2026-07-27 00:49:46 +0530

    Time: 0 ms (100%), Space: 11.6 MB (93.73%) - LeetHub

--- 0014-longest-common-prefix/0014-longest-common-prefix.cpp
+++ 0014-longest-common-prefix/0014-longest-common-prefix.cpp
@@ -1,18 +1,28 @@
 class Solution {
 public:
-    string longestCommonPrefix(vector<string>& str) {
-        if(str.empty()) return "";
-        sort(str.begin(), str.end());
-        string first = str[0];
-        string last = str[str.size()-1];
-        string ans = "";
-        int minLength = min(first.size(), last.size());
-        for(int i=0; i<minLength; i++){
-            if(first[i] != last[i]){
-                break;
+    string longestCommonPrefix(vector<string>& strs) {
+        // if(str.empty()) return "";
+        // sort(str.begin(), str.end());
+        // string first = str[0];
+        // string last = str[str.size()-1];
+        // string ans = "";
+        // int minLength = min(first.size(), last.size());
+        // for(int i=0; i<minLength; i++){
+        //     if(first[i] != last[i]){
+        //         break;
+        //     }
+        //     ans+=first[i];
+        // }
+        // return ans;
+        string temp = strs[0];
+        for (int i = 1; i < strs.size(); i++) {
+            for (int j = 0; j < temp.size(); j++) {
+                if (temp[j] != strs[i][j]) {
+                    temp.erase(j);
+                    break;
+                }
             }
-            ans+=first[i];
         }
-        return ans;
+        return temp;
     }
 };
