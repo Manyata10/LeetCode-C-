57d96390119c3e422d168e2c43e07b4dff5bbd4f
Manyata <130667631+Manyata10@users.noreply.github.com>
2026-07-28 13:24:16 +0530

    Time: 0 ms (100%), Space: 14.2 MB (68.76%) - LeetHub

--- 0349-intersection-of-two-arrays/0349-intersection-of-two-arrays.cpp
+++ 0349-intersection-of-two-arrays/0349-intersection-of-two-arrays.cpp
@@ -24,15 +24,31 @@ public:
 
     //using hashing - hashset
 
+    // vector<int> intersection(vector<int>& nums1, vector<int>& nums2) {
+    //     unordered_set<int> set1(nums1.begin(), nums1.end());
+    //     unordered_set<int> result;
+    //     for(int num : nums2){
+    //         if(set1.contains(num)){
+    //             result.insert(num);
+    //         }
+    //     }
+    //     return vector<int>(result.begin(), result.end()); 
+    // }
+
+    //using hashmap
     vector<int> intersection(vector<int>& nums1, vector<int>& nums2) {
-        unordered_set<int> set1(nums1.begin(), nums1.end());
-        unordered_set<int> result;
+        unordered_map<int, int> mpp;
+        for( int num : nums1){
+            mpp[num] = 1;
+        }
+        vector<int> result;
+
         for(int num : nums2){
-            if(set1.contains(num)){
-                result.insert(num);
+            if((mpp.find(num)!=mpp.end()) && mpp[num]==1){
+                result.push_back(num);
+                mpp[num] = 0;
             }
         }
-        return vector<int>(result.begin(), result.end()); 
+        return result;
     }
-
 };
