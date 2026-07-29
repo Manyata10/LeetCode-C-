e55e6c4d544c5fca222525021efda125b28046ad
Manyata <130667631+Manyata10@users.noreply.github.com>
2026-07-29 23:24:21 +0530

    Time: 48 ms (53.63%), Space: 29 MB (73.61%) - LeetHub

--- 0015-3sum/0015-3sum.cpp
+++ 0015-3sum/0015-3sum.cpp
@@ -1,32 +1,31 @@
 class Solution {
 public:
-    vector<vector<int>> threeSum(vector<int>& nums) {
+    vector<vector<int>> threeSum(vector<int>& arr) {
+        int n = arr.size();
+        sort(arr.begin(), arr.end());
         vector<vector<int>> ans;
-        sort(nums.begin(), nums.end());
-        int n = nums.size();
-        int i = 0;
-        for(int i=0;i<n-2;i++){
-            if(i>0 && nums[i] == nums[i-1]){
-                continue;
-            }
-            int left=i+1,right=n-1;
-            while(left < right) {
-            int sum = nums[left] + nums[right];
 
-                if(sum == -nums[i]) {
-                    ans.push_back({nums[i], nums[left], nums[right]});  // ⬅️ The push_back you asked for
-                    
-                    // skip duplicates
-                    while(left < right && nums[left] == nums[left+1]) left++;
-                    while(left < right && nums[right] == nums[right-1]) right--;
+        for(int i=0;i <n; i++){
+            if(i>0 && arr[i] == arr[i-1]) continue;
 
-                    left++;
-                    right--;
+            //Two Pointers
+            int left = i+1, right = n-1;
+
+            //Find Pairs for current arr[i]
+            while(left < right){
+                int sum = arr[i] + arr[left] + arr[right];
+                if(sum==0){
+                    ans.push_back({arr[i], arr[left], arr[right]});
+                    left++, right--;
+
+                    //skipp duplicates for left
+                    while(left < right && arr[left] == arr[left - 1]) left++;
+                    while(left < right && arr[right] == arr[right+1]) right--;
                 }
-                else if(sum < -nums[i]) left++;
+                else if(sum < 0) left++;
                 else right--;
             }
         }
-        return ans;
+    return ans;
     }
 };
