784ee74af27be53d4fb0f0d3b0a043bc64913dcb
Manyata <130667631+Manyata10@users.noreply.github.com>
2026-08-30 19:02:13 +0530

    Time: 427 ms (27.65%), Space: 328.7 MB (25.97%) - LeetHub

--- 1971-find-if-path-exists-in-graph/1971-find-if-path-exists-in-graph.cpp
+++ 1971-find-if-path-exists-in-graph/1971-find-if-path-exists-in-graph.cpp
@@ -1,13 +1,5 @@
 class Solution {
 public:
-    // void dfsHelper(int u, vector<bool>&vis){
-    //     vis[u] = true;
-    //     for(int v:l[u]){
-    //         if(!vis[v]){
-    //             dfsHelper(v, vis);
-    //         }
-    //     }
-    // }
     bool dfs(int n, vector<vector<int>>& adj, int src, int dest, vector<bool>& vis){
         if(src == dest) return true;
         vis[src]=true;
@@ -21,17 +13,7 @@ public:
                 }
             }
         }
-        // for(int i=0;i<edges.size();i++){
-        //     if(adj[i]==src){
-        //         if(edges[i][1]==dest){
-        //             return true;
-        //         }
-        //         else dfs(n, edges, edges[i][1], dest, vis);
-        //     }
-        // }
         return false;
-
-        // dfsHelper(src, vis);
     }
     bool validPath(int n, vector<vector<int>>& edges, int source, int destination) {
         vector<bool> vis(n, false);
