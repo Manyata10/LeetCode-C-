617cb36b450dd41da6b98fe157adf0797aae2b18
Manyata <130667631+Manyata10@users.noreply.github.com>
2026-08-31 18:07:22 +0530

    Time: 3 ms (25.56%), Space: 16.7 MB (86.25%) - LeetHub

--- 0994-rotting-oranges/0994-rotting-oranges.cpp
+++ 0994-rotting-oranges/0994-rotting-oranges.cpp
@@ -7,15 +7,17 @@ public:
         int min = 0;
 
         queue<pair<pair<int,int>,int>> Q;
-        vector<vector<bool>> vis(m,vector<bool>(n,false));
+        // vector<vector<bool>> vis(m,vector<bool>(n,false));
         for(int i=0; i<m; i++){
             for(int j = 0;j<n; j++){
                 if(grid[i][j] == 2){
                     Q.push({{i,j}, 0});
-                    vis[i][j] = true;
+                    // vis[i][j] = true;
                 }
             }
         }
+        int dr[] = {-1, 0, 1, 0};
+        int dc[] = {0, 1, 0, -1};
         // if(Q.size()==0){ return -1;}
         while(Q.size()>0){
             int i = Q.front().first.first;
@@ -24,27 +26,38 @@ public:
             Q.pop();
 
             min = max(min, time);
-            if(i-1 >=0 && !vis[i-1][j] && grid[i-1][j]==1){
-                Q.push({{i-1,j},time+1});
-                vis[i-1][j] = true;
-            }
-            if(j+1 < n && !vis[i][j+1] && grid[i][j+1]==1){
-                Q.push({{i, j+1}, time+1});
-                vis[i][j+1] = true;
-            }
-            if(i+1 < m && !vis[i+1][j] && grid[i+1][j] == 1){
-                Q.push({{i+1,j}, time+1});
-                vis[i+1][j] = true;
-            }
-            if(j-1 >=0 && !vis[i][j-1] && grid[i][j-1] == 1){
-                Q.push({{i,j-1}, time+1});
-                vis[i][j-1] = true;
+            // if(i-1 >=0 && !vis[i-1][j] && grid[i-1][j]==1){
+            //     Q.push({{i-1,j},time+1});
+            //     vis[i-1][j] = true;
+            // }
+            // if(j+1 < n && !vis[i][j+1] && grid[i][j+1]==1){
+            //     Q.push({{i, j+1}, time+1});
+            //     vis[i][j+1] = true;
+            // }
+            // if(i+1 < m && !vis[i+1][j] && grid[i+1][j] == 1){
+            //     Q.push({{i+1,j}, time+1});
+            //     vis[i+1][j] = true;
+            // }
+            // if(j-1 >=0 && !vis[i][j-1] && grid[i][j-1] == 1){
+            //     Q.push({{i,j-1}, time+1});
+            //     vis[i][j-1] = true;
+            // }
+            for(int k = 0; k<4;k++){
+                int nr = i+dr[k];
+                int nc = j+dc[k];
+                if(nr>=0 && nr < m && nc >= 0 && nc < n && grid[nr][nc] == 1){
+                    grid[nr][nc] = 2;
+                    Q.push({{nr, nc}, time+1});
+                }
             }
         }
         //check for fresh oranges
         for(int i=0; i<m;i++){
             for(int j=0;j<n;j++){
-                if(grid[i][j] == 1 && !vis[i][j]){
+                // if(grid[i][j] == 1 && !vis[i][j]){
+                //     return -1;
+                // }
+                if(grid[i][j] == 1){
                     return -1;
                 }
             }
