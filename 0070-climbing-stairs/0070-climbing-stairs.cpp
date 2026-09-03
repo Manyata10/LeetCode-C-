72d5e696f494465c9ed1ae771aa0a6ee0d8eea51
Manyata <130667631+Manyata10@users.noreply.github.com>
2026-09-03 17:35:38 +0530

    Time: 0 ms (100%), Space: 7.9 MB (59.85%) - LeetHub

--- 0070-climbing-stairs/0070-climbing-stairs.cpp
+++ 0070-climbing-stairs/0070-climbing-stairs.cpp
@@ -1,16 +1,44 @@
 class Solution {
 public:
-    int helper(int n, vector<int> &dp){
-        if(n==1 || n==2){
-            return n;
+
+//using memoization in DP
+    // int helper(int n, vector<int> &dp){
+    //     if(n==1 || n==2){
+    //         return n;
+    //     }
+    //     if(dp[n] !=-1){
+    //         return dp[n];
+    //     }
+    //     return dp[n]=helper(n-1, dp) + helper(n-2, dp);
+    // }
+    // int climbStairs(int n) {
+    //     vector<int> dp(n+1, -1);
+    //     return helper(n, dp);
+    // }
+
+//using tabulation
+
+    int climbStairs(int n){
+        if(n==1 || n==2) return n;
+
+        // vector<int> dp(n+1);
+        // dp[0] = 0;
+        // dp[1] = 1;
+        // dp[2] = 2; 
+        int prev1 = 2, prev2=1;
+
+        int result = prev1;
+
+        for(int i=3; i<n+1; i++){
+            // dp[i] = dp[i-1] + dp[i-2];
+            result = prev1+prev2;
+            prev2 = prev1;
+            prev1 = result;
+
         }
-        if(dp[n] !=-1){
-            return dp[n];
-        }
-        return dp[n]=helper(n-1, dp) + helper(n-2, dp);
+        // return dp[n];
+        return result;
     }
-    int climbStairs(int n) {
-        vector<int> dp(n+1, -1);
-        return helper(n, dp);
-    }
+
+
 };
