397754b3b6f603f40a3881aae94a9ef64ef14397
Manyata <130667631+Manyata10@users.noreply.github.com>
2026-09-05 02:18:28 +0530

    Time: 283 ms (32.06%), Space: 296.3 MB (29.75%) - LeetHub

---
+++ 0300-longest-increasing-subsequence/0300-longest-increasing-subsequence.cpp
@@ -0,0 +1,56 @@
+class Solution {
+public:
+
+    int solve(int n, vector<int> &nums, int curr, int prev){
+        if(curr==n){
+            return 0;
+        }
+        int take = 0;
+        if(prev == -1 || nums[curr]> nums[prev]){
+            take = 1 + solve(n, nums, curr+1, curr);
+        }
+        int notTake = solve(n, nums, curr+1, prev);
+        return max(take, notTake);
+    }
+
+    int solveMem(int n, vector<int> &nums, int curr, int prev, vector<vector<int>> &dp){ //onluy passes indexes
+        //base case
+        if(curr==n){
+            return 0;
+        }
+        if(dp[curr][prev+1]!=-1){
+            return dp[curr][prev+1];
+        }
+        int take = 0;
+        if(prev == -1 || nums[curr]> nums[prev]){
+            take = 1 + solveMem(n, nums, curr+1, curr, dp);
+        }
+        int notTake = solveMem(n, nums, curr+1, prev, dp);
+        return dp[curr][prev+1] = max(take, notTake);
+    }
+
+    int solveTab(int n, vector<int> &nums){
+        vector<vector<int>> dp(n+1, vector<int>(n+1,0));
+        for(int curr=n-1; curr >=0; curr--){
+            for(int prev = curr-1; prev>=-1; prev--){
+                int take = 0;
+                if(prev == -1 || nums[curr] > nums[prev]){
+                    take = 1 + dp[curr+1][curr+1];
+                }
+                int notTake = 0 + dp[curr+1][prev+1];
+
+                dp[curr][prev+1] = max(take, notTake);
+            }
+        }
+        return dp[0][0];
+    }
+
+    int lengthOfLIS(vector<int>& nums) {
+        int n = nums.size();
+        // return solve(n,nums, 0, -1);
+        // vector<vector<int>> dp(n, vector<int>(n+1, -1)); //curr : 0 -> n-1, prev : -1 -> n-1
+        // solveMem(n, nums, 0, -1, dp);
+
+        return solveTab(n, nums);
+    }
+};
