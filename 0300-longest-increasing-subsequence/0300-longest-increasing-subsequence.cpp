fe5fed2e0e5697287989fe683d91fcae48a4b459
Manyata <130667631+Manyata10@users.noreply.github.com>
2026-09-05 17:59:21 +0530

    Time: 0 ms (100%), Space: 14.3 MB (61.25%) - LeetHub

--- 0300-longest-increasing-subsequence/0300-longest-increasing-subsequence.cpp
+++ 0300-longest-increasing-subsequence/0300-longest-increasing-subsequence.cpp
@@ -2,56 +2,56 @@ class Solution {
 public:
 
 
-    int solve(int n, vector<int> &nums, int curr, int prev){
-        if(curr==n){
-            return 0;
-        }
-        int take = 0;
-        if(prev == -1 || nums[curr]> nums[prev]){
-            take = 1 + solve(n, nums, curr+1, curr);
-        }
-        int notTake = solve(n, nums, curr+1, prev);
-        return max(take, notTake);
-    }
+    // int solve(int n, vector<int> &nums, int curr, int prev){
+    //     if(curr==n){
+    //         return 0;
+    //     }
+    //     int take = 0;
+    //     if(prev == -1 || nums[curr]> nums[prev]){
+    //         take = 1 + solve(n, nums, curr+1, curr);
+    //     }
+    //     int notTake = solve(n, nums, curr+1, prev);
+    //     return max(take, notTake);
+    // }
 
-    int solveMem(int n, vector<int> &nums, int curr, int prev, vector<vector<int>> &dp){ //onluy passes indexes
-        //base case
-        if(curr==n){
-            return 0;
-        }
-        if(dp[curr][prev+1]!=-1){
-            return dp[curr][prev+1];
-        }
-        int take = 0;
-        if(prev == -1 || nums[curr]> nums[prev]){
-            take = 1 + solveMem(n, nums, curr+1, curr, dp);
-        }
-        int notTake = solveMem(n, nums, curr+1, prev, dp);
-        return dp[curr][prev+1] = max(take, notTake);
-    }
+    // int solveMem(int n, vector<int> &nums, int curr, int prev, vector<vector<int>> &dp){ //onluy passes indexes
+    //     //base case
+    //     if(curr==n){
+    //         return 0;
+    //     }
+    //     if(dp[curr][prev+1]!=-1){
+    //         return dp[curr][prev+1];
+    //     }
+    //     int take = 0;
+    //     if(prev == -1 || nums[curr]> nums[prev]){
+    //         take = 1 + solveMem(n, nums, curr+1, curr, dp);
+    //     }
+    //     int notTake = solveMem(n, nums, curr+1, prev, dp);
+    //     return dp[curr][prev+1] = max(take, notTake);
+    // }
 
-    int solveTab(int n, vector<int> &nums){
-        // vector<vector<int>> dp(n+1, vector<int>(n+1,0));
+    // int solveTab(int n, vector<int> &nums){
+    //     // vector<vector<int>> dp(n+1, vector<int>(n+1,0));
 
-        vector<int> currRow(n+1, 0);
-        vector<int> nextRow (n+1,0);
+    //     vector<int> currRow(n+1, 0);
+    //     vector<int> nextRow (n+1,0);
 
 
 
-        for(int curr=n-1; curr >=0; curr--){
-            for(int prev = curr-1; prev>=-1; prev--){
-                int take = 0;
-                if(prev == -1 || nums[curr] > nums[prev]){
-                    take = 1 + nextRow[curr+1];
-                }
-                int notTake = 0 + nextRow[prev+1];
+    //     for(int curr=n-1; curr >=0; curr--){
+    //         for(int prev = curr-1; prev>=-1; prev--){
+    //             int take = 0;
+    //             if(prev == -1 || nums[curr] > nums[prev]){
+    //                 take = 1 + nextRow[curr+1];
+    //             }
+    //             int notTake = 0 + nextRow[prev+1];
 
-                currRow[prev+1] = max(take, notTake);
-            }
-            nextRow = currRow;
-         }
-        return nextRow[0];
-    }
+    //             currRow[prev+1] = max(take, notTake);
+    //         }
+    //         nextRow = currRow;
+    //      }
+    //     return nextRow[0];
+    // }
 
     int solveOptimal(int n, vector<int> nums){
         if(n==0){
